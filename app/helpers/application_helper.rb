63d4da17db124b51effa8051a4c66bbc24a0ad0d
Quang Duc <quang.duc@eastagile.com>
2014-12-30 22:14:07 +0700

    add link to Facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def facebook_url(site)
+    if site.graphic?
+      "https://www.facebook.com/pages/ADOH-Graphic-Design-Agency/301587420040130"
+    else
+      "https://www.facebook.com/pages/ADOH-Decor-Thi%E1%BA%BFt-k%E1%BA%BF-Thi-c%C3%B4ng-N%E1%BB%99i-th%E1%BA%A5t-g%E1%BB%97-Pallet/1534718423409559"
+    end
+  end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,16 @@
 class Site < ActiveRecord::Base
   has_many :posts, inverse_of: :site
   has_many :albums, inverse_of: :site
+
+  def graphic?
+    title == "Graphic Design"
+  end
+
+  def interior?
+    title == "Interior Design"
+  end
+
+  def pallet?
+    title == "Pallet Wood Design"
+  end
 end
